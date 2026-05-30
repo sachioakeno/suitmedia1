fff8c18ed59914ce6f1568a289900df7c4bb554f
sachioakeno <sachioakenochan@gmail.com>
2026-05-30 19:50:17 +0700

    adsadda

--- suitmedia-ideas/src/setupProxy.js
+++ suitmedia-ideas/src/setupProxy.js
@@ -5,4 +5,16 @@ module.exports = function(app) {
     target: 'https://suitmedia-backend.suitdev.com',
     changeOrigin: true,
   }));
+
+  app.use('/storage', createProxyMiddleware({
+    target: 'https://assets.suitdev.com',
+    changeOrigin: true,
+    onProxyReq: function(proxyReq) {
+      proxyReq.removeHeader('Origin');
+      proxyReq.removeHeader('Referer');
+      proxyReq.setHeader('Origin', 'https://suitmedia.com');
+      proxyReq.setHeader('Referer', 'https://suitmedia.com/');
+      proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
+    }
+  }));
 };
